bf4db4acd29929c502004aeede2fd902f065216d
Sabricetin <sabriyazilim@gmail.com>
2025-02-08 23:58:06 +0300

    Contraints
    İnbox View Controller Contraints

--- Chat-App/Controller/ProfileViewController.swift
+++ Chat-App/Controller/ProfileViewController.swift
@@ -6,16 +6,47 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class ProfileViewController: UIViewController {
 
+    /*
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getUserInfo()
     }
     
-
+    func getUserInfo() {
+        let databaseRef = Database.database().reference()
+        let auth = Auth.auth()
+        
+        databaseRef.child(Child.USERS)
+            .queryOrdered(byChild: "uid")
+            .queryEqual(toValue: auth.currentUser!.uid)
+            .observeSingleEvent(of: .value) { dataSnapshot in
+                for child in dataSnapshot.children{
+                    let snap = child as! DataSnapshot
+                    let userInfo = UserInfo(snapshot: snap)
+                    
+                    self.infoLabel.text = userInfo.name
+                    Helper.imageLoad(imageView: self.photoImageView, url: userInfo.photoUrl!)
+                }
+            }
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +56,5 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+*/
 }
